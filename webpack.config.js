4e32874c37e48ed89e33d530ef100b76df7805c9
Mykchailo <mishagazin85@gmail.com>
2024-10-07 13:00:50 +0300

    homework35

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const test = require("node:test");
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 
 module.exports = {
@@ -13,17 +17,42 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    target: 'web',
+
+    devServer: {
+        port: 3000,
+        hot: false
+    },
     module: {
         rules: [
+
             {
-                test: /\.js$/, // Обробка JS файлів
+                test: /\.ts$/, // Обробка JS файлів
                 exclude: /node_modules/, // Виняток для node_modules
-                use: 'babel-loader', // Лоадер для Babel
+                use: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-typescript'
+                    ]
+                }
+
             },
             {
-                test: /\.scss$/, // Обробка SCSS файлів
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.s[ac]ss$/, // Обробка SCSS файлів
+                use:[
+                    {loader: MiniCssExtractPlugin.loader,
+                        options: {
+                        publicPath: '',
+                        },
+                    },
+                    'css-loader',
+                    'less-loader',
+                    'sass-loader',
+                ],
             },
+
             {
                 test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
                 type: 'asset/resource',
@@ -39,6 +68,16 @@ module.exports = {
             template: './src/index.html', // Шлях до твого шаблону HTML
             filename: 'index.html', // Ім'я згенерованого HTML файлу в папці dist
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new EslintWebpackPlugin({
+            extensions: ['js'],
+            fix: true
+        }),
+         new BundleAnalyzerPlugin(),
+
     ],
 };
+
 
